21babc774cc10fcd90af8118589f5117a4662165
UniqeXD <shubhogarai730@gmail.com>
2025-05-21 09:59:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ImperfectXD/Sohinixmusic",
+    "https://github.com/UniqeXD/Kismissmusic-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
